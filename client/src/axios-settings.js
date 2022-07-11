3200630b73e374ba40ee4621b150893983f3ba45
vanthieu97 <vanthieunguyen234@gmail.com>
2022-07-11 21:15:15 +0700

    custom baseURL for axios

--- client/src/axios-settings.js
+++ client/src/axios-settings.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3018/api',
+  baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:3018/api' : '',
   withCredentials: true,
 })
 
