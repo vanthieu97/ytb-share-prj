acfd1b75430aa6bf2417015c82bf9ce3eb13c8fc
vanthieu97 <vanthieunguyen234@gmail.com>
2022-07-11 20:52:09 +0700

    deploy

--- config/db/index.js
+++ config/db/index.js
@@ -2,14 +2,10 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
   try {
-    // await mongoose.connect('mongodb://localhost:27017/remitano', {
-    await mongoose.connect(
-      'mongodb+srv://vanthieu97:thieu1997@cluster0-nzbrz.gcp.mongodb.net/remitano?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      },
-    )
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     console.log('Connect successfully!')
   } catch (error) {
     console.log('Connect failure!', error)
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const path = require('path')
 const express = require('express')
 const session = require('express-session')
@@ -8,9 +9,9 @@ const routes = require('./routes')
 const db = require('./config/db')
 
 db.connect()
-
+console.log(process.env)
 const app = express()
-const port = 3018
+const port = process.env.PORT || 3018
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -18,29 +19,29 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(
   session({
-    secret: 'REMITANO',
+    secret: process.env.SECRET_KEY,
     cookie: { maxAge: 1000 * 60 * 60 },
+  }),
+)
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionSuccessStatus: 200,
   }),
 )
 
 routes(app)
 
-app.use(morgan('combined'))
+// app.use(morgan('combined'))
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client', 'build')))
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
   })
-} else {
-  app.use(
-    cors({
-      origin: 'http://localhost:3000',
-      credentials: true,
-      optionSuccessStatus: 200,
-    }),
-  )
 }
 
 app.listen(port, () => console.log(`App listen at http://localhost:${port}`))
